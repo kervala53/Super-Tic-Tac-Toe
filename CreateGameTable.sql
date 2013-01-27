f8bf21ac451bb1c5b8060f002eb5be64243abf97
zurachakve <zurachakve@gmail.com>
2013-01-27 17:15:37 +0400

    change create game table sql.

--- CreateGameTable.sql
+++ CreateGameTable.sql
@@ -1,1 +1,1 @@
-Create table game (game_id int AUTO_INCREMENT,board varchar(81),player1_id int,player2_id int,turn int,isStarted int,isFinished int,whoWon int,primary key(game_id ));
+Create table game (game_id int AUTO_INCREMENT,board varchar(81),player1_id int,player2_id int,turn int,isStarted boolean default 0,isFinished boolean default 0,whoWon int,primary key(game_id ));

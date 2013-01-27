5b8e6b7ec8a2c673fd27a71133d765e2b5b3a535
giorgi kervalishvili <gkerv10@freeuni.edu.ge>
2013-01-27 22:20:59 +0400

    mysql bugic gascorda

--- sitCreate.php
+++ sitCreate.php
@@ -28,16 +28,26 @@ if(isset($_POST["game"])){ // jdeba sxvastan satamashod
 		die ("Can not use tictactoe base : " . mysql_error());
 	}
 	$user_id =  $_SESSION["user_id"];
-	$querry = mysql_query("update game set player2_id=".$user_id." ,isStarted=1  where player2_id ='0',game_id='".$game_id."'");
+	
+	$querry = mysql_query("select * from game where game_id =".$game_id.";", $conection);
+
+	if (!$querry) {
+		die(mysql_error());
+	}
+	$row = mysql_fetch_array($querry);
+	$player1_id = $row["player1_id"]; 
+	 
+	$querry = mysql_query("update game set player2_id=".$user_id." ,isStarted=1 ,turn=".$player1_id." where player2_id ='0' and game_id =".$game_id."");
 	if (!$querry) {
 		die("Could not update querry: " . mysql_error());
 	}
-	if(mysql_num_rows($querry)==0){
-		echo "Some one has sat on this table ! ";
-		header("refresh:5; url=mainPage.php");
+	if($querry>0){
+		header("Location:play.php");
+	//	header("refresh:5; url=mainPage.php");
 	}
 	else{
-		header("Location:play.php");
+		echo "Some one has sat on this table ! ";
+		
 	}
 		
 }

5c95534d5a6e690cd461f5af2b1c594f7dbdd995
giorgi kervalishvili <gkerv10@freeuni.edu.ge>
2013-01-27 17:21:17 +0400

    patra monaxazi

--- mainPage.php
+++ mainPage.php
@@ -1,4 +1,27 @@
 <?php
-echo "<a href= \"userStatistics.php\"> User statistics </a>";
-echo "<a href= \"userSettings.php\"> User settings </a>";
+	session_start();
+	if(isset($_SESSION["user_id"])){ 
+		$user_id = $_SESSION['user_id'];
+	}
+	
+	else{
+		header("Location:index.php");
+	}
+	$conection = mysql_connect('localhost', 'root', '');
+	if (!$conection) {
+		die("Could not connect: ". mysql_error());
+	}
+	
+	if (!mysql_select_db('tictactoe', $conection)) {
+	    die ("Can not use tictactoe base : " . mysql_error());
+	}
+
+	$result = mysql_query("select * from user where user_id = '".$user_id."';", $conection);
+
+	if (!$result) {
+			die(mysql_error());
+	echo "<a href= \"userStatistics.php\"> User statistics </a>";
+	echo "<a href= \"userSettings.php\"> User settings </a>";
+
 ?>
+

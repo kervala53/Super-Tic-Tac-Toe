70b45bf9ab6525aacbc9b80cf7f3b27385d22c19
zurachakve <zurachakve@gmail.com>
2013-01-27 15:23:45 +0400

    drawing board, x,y, bigX, bigY, clickhandler

---
+++ drawingcanvas.js
@@ -0,0 +1,145 @@
+var xBoard = 0;
+var oBoard = 0;
+var begin = true;
+var context;
+var width, height;
+var size = 9;
+
+function paintBoard() {
+	var board = document.getElementById('board');
+
+	width = board.width;
+	height = board.height;
+	context = board.getContext('2d');
+
+	context.beginPath();
+	context.strokeStyle = '#000';
+	context.lineWidth = 4;
+
+	var size = 9;
+	for(var i=0; i<=size; i++){
+		context.moveTo((width / size)*i, 0);
+		context.lineTo((width / size)*i, height);
+
+		context.moveTo(0, (height / size)*i);
+		context.lineTo(width, (height / size)*i);
+	}
+
+
+	context.stroke();
+	context.closePath();
+
+	// if (begin) {
+		// var ini = Math.abs(Math.floor(Math.random() * 9 - 0.1));
+		// markBit(1 << ini, 'O');
+		// begin = false;
+	// } else {
+		// begin = true;
+	// }
+}
+
+
+function paintX(x, y) {
+
+	context.beginPath();
+
+	context.strokeStyle = '#ff0000';
+	context.lineWidth = 4;
+
+	var offsetX = (width / size) * 0.1;
+	var offsetY = (height / size) * 0.1;
+
+	var beginX = x * (width / size) + offsetX;
+	var beginY = y * (height / size) + offsetY;
+
+	var endX = (x + 1) * (width / size) - offsetX;
+	var endY = (y + 1) * (height / size) - offsetY;
+
+	context.moveTo(beginX, beginY);
+	context.lineTo(endX, endY);
+
+	context.moveTo(beginX, endY);
+	context.lineTo(endX, beginY);
+
+	context.stroke();
+	context.closePath();
+}
+
+function paintO(x, y) {
+
+	context.beginPath();
+
+	context.strokeStyle = '#0000ff';
+	context.lineWidth = 4;
+
+	var offsetX = (width / size) * 0.1;
+	var offsetY = (height / size) * 0.1;
+
+	var beginX = x * (width / size) + offsetX;
+	var beginY = y * (height / size) + offsetY;
+
+	var endX = (x + 1) * (width / size) - offsetX;
+	var endY = (y + 1) * (height / size) - offsetY;
+
+	context.arc(beginX + ((endX - beginX) / 2), beginY + ((endY - beginY) / 2), (endX - beginX) / 2, 0, Math.PI * 2, true);
+
+	context.stroke();
+	context.closePath();
+}
+
+function paintBigX(x, y) {
+
+	context.beginPath();
+
+	context.strokeStyle = '#ff0000';
+	context.lineWidth = 4;
+
+	var offsetX = (width / 3) * 0.1;
+	var offsetY = (height / 3) * 0.1;
+
+	var beginX = x * (width / 3) + offsetX;
+	var beginY = y * (height / 3) + offsetY;
+
+	var endX = (x + 1) * (width / 3) - offsetX;
+	var endY = (y + 1) * (height / 3) - offsetY;
+
+	context.moveTo(beginX, beginY);
+	context.lineTo(endX, endY);
+
+	context.moveTo(beginX, endY);
+	context.lineTo(endX, beginY);
+
+	context.stroke();
+	context.closePath();
+}
+
+function paintBigO(x, y) {
+
+	context.beginPath();
+
+	context.strokeStyle = '#0000ff';
+	context.lineWidth = 4;
+
+	var offsetX = (width / 3) * 0.1;
+	var offsetY = (height / 3) * 0.1;
+
+	var beginX = x * (width / 3) + offsetX;
+	var beginY = y * (height / 3) + offsetY;
+
+	var endX = (x + 1) * (width / 3) - offsetX;
+	var endY = (y + 1) * (height / 3) - offsetY;
+
+	context.arc(beginX + ((endX - beginX) / 2), beginY + ((endY - beginY) / 2), (endX - beginX) / 2, 0, Math.PI * 2, true);
+
+	context.stroke();
+	context.closePath();
+}
+
+function clickHandler(e) {
+
+	var y = Math.floor(e.clientY / (height / size));
+	var x = Math.floor(e.clientX / (width / size));
+
+	paintX(x,y);
+
+}

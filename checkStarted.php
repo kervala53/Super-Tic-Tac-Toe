bfe419ddb996eacf547eb4e32050eaa8c60fe41a
zurachakve <zurachakve@gmail.com>
2013-01-27 22:57:37 +0400

    finish. works ok

--- checkStarted.php
+++ checkStarted.php
@@ -3,8 +3,7 @@ session_start();
 if (!isset($_SESSION["user_id"])) {
 	header("Location:index.php");
 }
-
-if (isset($_SESSION["table_id"])) {
+if (isset($_SESSION["game_id"])) {
 	$conection = mysql_connect('localhost', 'root', '');
 	if (!$conection) {
 		die("Could not connect: " . mysql_error());
@@ -12,16 +11,14 @@ if (isset($_SESSION["table_id"])) {
 	if (!mysql_select_db('tictactoe', $conection)) {
 		die("Can not use test base : " . mysql_error());
 	}
-
-	$arr = mysql_query("select * from game where table_id = ".$_SESSION["table_id"]." and player1_id = ".$_SESSION["user_id"]." and player2_id != 0;", $conection);
-
+	$arr = mysql_query("select * from game where game_id = ".$_SESSION["game_id"]." and player1_id = ".$_SESSION["user_id"]." and player2_id != 0;", $conection);
 	if (!$arr) {
 		die(mysql_error());
 	}
 	if (mysql_num_rows($arr) > 0) {
-		return 1;
+		echo 1;
 	} else {
-		return 0;
+		echo 0;
 	}
 }
 ?>

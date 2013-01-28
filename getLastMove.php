759ac75dd378a10231843de8e4f1df10a19d6790
zurachakve <zurachakve@gmail.com>
2013-01-28 12:46:10 +0400

    add file getlastmove. it uses user id and game id to return last move. if game is not started it returns 0, if started it returns 3char size string. first is player 1 or 2, second is x and thirs is y coordinate

---
+++ getLastMove.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+if (!isset($_SESSION["user_id"])) {
+	header("Location:index.php");
+}
+if (isset($_SESSION["game_id"])) {
+	$conection = mysql_connect('localhost', 'root', '');
+	if (!$conection) {
+		die("Could not connect: " . mysql_error());
+	}
+	if (!mysql_select_db('tictactoe', $conection)) {
+		die("Can not use test base : " . mysql_error());
+	}
+	$arr = mysql_query("select * from game where game_id = ".$_SESSION["game_id"]." and turn = ".$_SESSION["user_id"].";", $conection);
+	if (!$arr) {
+		die(mysql_error());
+	}
+	if (mysql_num_rows($arr) > 0) {
+		$row = mysql_fetch_array($arr);
+		echo $row['last_move'];
+	} else {
+		echo 0;
+	}
+	
+}
+?>

5a77453471936ab05b2c3612f1ac5f2d5aec0c2f
giorgi kervalishvili <gkerv10@freeuni.edu.ge>
2013-01-28 13:46:56 +0400

    shemoecmeba diagonalebis garda

--- checking.php
+++ checking.php
@@ -1,31 +1,48 @@
 <?php
 $patara = "000100000";
-$didi = "000000000000000000000000000000000000000000000000000000000000000000000000000000000";
-for($i=0;$i<3;$i++){
-	for($i=0;$i<9;$i++){
-		if($patara{$i}==1 || $patara{$i}==2){
-			$i++;
-		}
-		$didi{$j*$i};
-	}
-}
+// $didi = "000000000000000000000000000000000000000000000000000000000000000000000000000000000";
+// for($i=0;$i<3;$i++){
+	// for($i=0;$i<9;$i++){
+		// if($patara{$i}==1 || $patara{$i}==2){
+			// $i++;
+		// }
+		// $didi{$j*$i};
+	// }
+// }
 $won = false;
 $whowon =5;
 for($i=0;$i<3;$i++){
 	for($j=0;$j<3;$j++){
-		if($j=0){			
-			if($patara{$i*3+$j}==0 && $patara{$i*3+$j+1}==0 && $patara{$i*3+$j+2}==0){
-				$won = true;
-				$whowon =0;
+		if($j==0){
+			if($patara{$i*3+$j}==0){
+				if($patara{$i*3+$j+1}==0 && $patara{$i*3+$j+2}==0){
+					$won = true;
+					$whowon =0;
+				}
 			}
-			if($patara{$i*3+$j}==1 && $patara{$i*3+$j+1}==1 && $patara{$i*3+$j+2}==1){
-				$won = true;
-				$whowon =0;
+			else{
+				if($patara{$i*3+$j+1}==1 && $patara{$i*3+$j+2}==1){
+					$won = true;
+					$whowon =1;
+				}
+			}		
+		}
+		if($i==0){
+			if($patara{$j}==0){
+				if($patara{$j+3}==0 && $patara{$j+6}==0){
+					$won = true;
+					$whowon =0;
+				}
+			}
+			else{
+				if($patara{$j+3}==1 && $patara{$j+6}==1){
+					$won = true;
+					$whowon =1;
+				}
 			}
 		}	
 	}
 }
-$patara
 
 
 ?>

629725f7e4d464a294a7bfe476f125ce8d065213
zurachakve <zurachakve@gmail.com>
2013-01-28 12:48:29 +0400

    it is for player who creates game. it needs user id and table id. it waits for another player, makes move, waits for it turn and makes move an so on. need to add checking finish and other things

--- play.php
+++ play.php
@@ -21,42 +21,123 @@
 	<body>
 		<canvas height="500" width="500" id="board"></canvas>
 
-	<?php
+		<?php
 		session_start();
-		if(!isset($_SESSION["user_id"])){
+		if (!isset($_SESSION["user_id"])) {
 			header("Location:index.php");
 		}
-	?>
+		?>
 
 		<script>
 			$(document).ready(function() {
-				var isStarted = false; 
+				var isStarted = false;
+				var myTurn = false;
 
 				function isGameStarted() {
 					$.ajax({
-						url: "checkStarted.php",
-						type: 'POST',
-						async: false,
-						cache: false,
-						timeout: 10000,
-						complete: function(result) {
-							if(result.responseText == true){
+						url : "checkStarted.php",
+						type : 'POST',
+						async : false,
+						cache : false,
+						timeout : 10000,
+						complete : function(result) {
+							if (result.responseText == true) {
 								console.log("shemogviertda");
-								isStarted = true;	
-							}
-							else{
+								isStarted = true;
+								myTurn = true;
+							} else {
 								console.log("ar shemogviertda");
-								setTimeout(isGameStarted, 1000);	
+								setTimeout(isGameStarted, 1000);
 							}
 						}
-					})
+					});
 				};
 				isGameStarted();
-				console.log("aq movida");
+
 				paintBoard('');
+
+				function getLastMove() {
+					$.ajax({
+						url : "getLastMove.php",
+						type : 'POST',
+						async : false,
+						cache : false,
+						timeout : 10000,
+						complete : function(result) {
+							if (result.responseText.charAt(0) == o) {
+								paintO(result.charAt(1), result.charAt(2));
+							}
+						}
+					});
+				}
+
+				function isMyTurn() {
+					if (!isStarted) {
+						setTimeout(isMyTurn, 5000);
+						return;
+					}
+					$.ajax({
+						url : "getTurn.php",
+						type : 'POST',
+						async : false,
+						cache : false,
+						timeout : 10000,
+						complete : function(result) {
+							if (result.responseText == true) {
+								console.log("chemi jeria");
+								myTurn = true;
+								getLastMove();
+							} else {
+								console.log("araa chemi jeri");
+								setTimeout(isMyTurn, 3000);
+							}
+						}
+					});
+				};
+
+				function clickHandler(e, player) {
+
+					var tempY = Math.floor((e.clientY - 10) / (height / size));
+					var tempX = Math.floor((e.clientX - 10) / (width / size));
+
+					$.when($.ajax({
+						url : "isLegalMove.php",
+						type : 'POST',
+						data : {
+							'player' : player,
+							'x' : tempX,
+							'y' : tempY
+						},
+						async : false,
+						cache : false,
+						timeout : 10000,
+						complete : function(result) {
+							console.log(result.responseText);
+							return result.responseText;
+						}
+					})).then(function(isLegal, a, b) {
+						if (isLegal != 0) {
+							if (player == x) {
+								paintX(tempX, tempY);
+							} else {
+								paintO(tempX, tempY);
+							}
+							myTurn = false;
+							isMyTurn();
+						}
+					});
+				}
+
+
 				$('#board').click(function(e) {
-					clickHandler(e);
+					if (myTurn) {
+						//x agebulia drawingcanvas.js-dan
+						if (clickHandler(e, x)) {
+							isMyTurn();
+						}
+					}
 				});
+
 			});
 		</script>
 	</body>

6bbd17f737e01e932d04aabed42eb52734527134
giorgi kervalishvili <gkerv10@freeuni.edu.ge>
2013-01-29 05:59:59 +0400

    illegal svlebi
    daxurul magidas rom ar gadaaceros musahobs ushecdomod :*

--- isLegalMove.php
+++ isLegalMove.php
@@ -26,9 +26,49 @@ if (isset($_SESSION["game_id"])) {
 		$board = $row['board'];
 		$index = $y * 9 + $x;
 		$playerid = NULL;
-		if ($board[$index] != 0) {
+		$nashti = $index%9;
+		$ganakopi = (int)($index/9);
+		$dapa;
+		if($ganakopi<3){ // daxurul magidas rom ar gadaaceros 
+			if($nashti<3){
+				$dapa =0;
+			}
+			if($nashti>2 && $nashti<6){
+				$dapa =1;
+			}
+			if($nashti>5 && $nashti<9){
+				$dapa =2;	
+			}
+		}
+		if($ganakopi>2 && $ganakopi<6){
+			if($nashti<3){
+				$dapa =3;
+			}
+			if($nashti>2 && $nashti<6){
+				$dapa =1+3;
+			}
+			if($nashti>5 && $nashti<9){
+				$dapa =2+3;	
+			}
+		}
+		if($ganakopi>5 && $ganakopi<9){
+			if($nashti<3){
+				$dapa =6;
+			}
+			if($nashti>2 && $nashti<6){
+				$dapa =1+6;
+			}
+			if($nashti>5 && $nashti<9){
+				$dapa =2+6;	
+			}
+		}
+		if ($board[$index] != 0) { // natamashebs roma r gadaaceros 
 			echo 0;
-		} else {
+		}
+		else if($mini_board[$dapa]!=0 ){
+			echo 0;
+		}
+		else {
 			if ($_SESSION["user_id"] == $player1_id) {
 				$playerid = $player2_id;
 			} else {
